e48b24f90bc7c90b1e4318e65ff610402ed537b6
BryanWASD <brian.zaz.205.gh@gmail.com>
2024-07-01 16:11:12 -0600

    added cors

--- index.js
+++ index.js
@@ -3,11 +3,16 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/questions');
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
